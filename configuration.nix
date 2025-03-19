0c7a9fd0acb13dd35c4576408378d67dfc4c15ab
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-03-19 18:55:11 +0100

    style

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,12 @@
-{ config, pkgs, lib, ... }:
-
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
@@ -40,7 +42,7 @@
   # Enable the XFCE Desktop Environment.
   # services.xserver.displayManager.lightdm.enable = true;
   # services.xserver.desktopManager.xfce.enable = true;
-  
+
   # Enable the LXQT Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.lxqt.enable = true;
@@ -65,7 +67,7 @@
   users.users.tag = {
     isNormalUser = true;
     description = "tag";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [];
   };
 
@@ -96,25 +98,25 @@
   systemd.tmpfiles.rules = [
     "d /home/tag/.config/autostart/ 0777 tag - - -"
     ''
-    f /home/tag/.config/autostart/plg-connect.desktop 0777 tag - - [Desktop Entry]\nType=Application\nVersion=1.0\nOnlyShowIn=LXQt;\nName=PLGConnect\nPath=/home/tag/PLG-Connect/PLG-Connect-Presenter\nLD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH\nExec=dotnet run
+      f /home/tag/.config/autostart/plg-connect.desktop 0777 tag - - [Desktop Entry]\nType=Application\nVersion=1.0\nOnlyShowIn=LXQt;\nName=PLGConnect\nPath=/home/tag/PLG-Connect/PLG-Connect-Presenter\nLD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH\nExec=dotnet run
     ''
   ];
 
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
-  	 featherpad
-  	 git
-  
-     dotnet-sdk_9
-     dotnet-runtime_9
-     dotnet-aspnetcore_9
-     wmctrl
-  ]; 
- 
+    featherpad
+    git
+
+    dotnet-sdk_9
+    dotnet-runtime_9
+    dotnet-aspnetcore_9
+    wmctrl
+  ];
+
   # PLG Connect Updater
   systemd.services.plg-connect-updater = {
-    after = [ "network.target" ];
+    after = ["network.target"];
     serviceConfig = {
       User = "tag";
       Type = "oneshot";
@@ -122,42 +124,42 @@
     };
     path = [pkgs.git];
     startAt = "1h";
-    script = ''        
+    script = ''
       folder="PLG-Connect"
-        
+
       if ! test -d $folder; then
         git clone https://github.com/PLG-Development/PLG-Connect.git
       fi
-        
+
       pushd $folder
-        
+
       git pull
-        
+
       git switch production
-        
+
       popd
     '';
   };
-  
+
   # System Updater
   systemd.services.system-updater = {
-    after = [ "network.target" ];
+    after = ["network.target"];
     serviceConfig = {
       Type = "oneshot";
       WorkingDirectory = "/etc/nixos";
     };
-    path = [ pkgs.git pkgs.nixos-rebuild ];
+    path = [pkgs.git pkgs.nixos-rebuild];
     startAt = "1h";
     script = ''
       if ! git rev-parse --is-inside-work-tree; then
         git clone https://github.com/PLG-Development/PLG-Connect-NixOS .
       fi
-         
+
       git fetch
-      
+
       local_commit=$(git rev-parse main)
       remote_commit=$(git rev-parse origin/main)
-      
+
       if [ "$local_commit" != "$remote_commit" ]; then
         git pull
         nixos-rebuild boot
@@ -178,5 +180,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
